2496be6e07c3846a1c862f2385c87b1f6c57de41
ShibhamKumarGupta <shivamlovesu2forever@gmail.com>
2025-02-28 20:45:10 +0530

    Started my Project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <a href="https://stylustechnepal.com" target="_blank">anuzbvbmaniac123@gmail.com</a>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/ImageBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImageBox = () => {
+  return (
+    <div>
+      <div class="imgBx">
+            <img src="https://github.com/anuzbvbmaniac/Responsive-Product-Card---CSS-ONLY/blob/master/assets/img/jordan_proto.png?raw=true" alt="Nike Jordan Proto-Lyte Image"/>
+        </div>
+    </div>
+  )
+}
+
+export default ImageBox
